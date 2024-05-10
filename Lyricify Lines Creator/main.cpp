c0fae8908c4ea143d7974c27333582660ecb7f47
XY Wang <wxy11787@foxmail.com>
2024-05-10 11:44:19 +0800

    Add notes

--- Lyricify Lines Creator/main.cpp
+++ Lyricify Lines Creator/main.cpp
@@ -1,13 +1,14 @@
 #include "HiEasyX.h"
-#include <map>
 
 int main()
 {
+	// 初始化窗口
 	hiex::Window wnd(640, 480);
 	hiex::AutoExit();
 
 	HWND hwnd = wnd.GetHandle();
 
+	// 关闭窗口，结束程序
 	hiex::init_end(hwnd);
 
 	closegraph();
