f431645057195b64dbd12b15bb22b58ed2d82c8e
XY Wang <wxy11787@foxmail.com>
2024-05-12 16:18:50 +0800

    [Change] Use class instead of namespace and add wstring support

--- Lyricify Lines Creator/StringHelper.cpp
+++ Lyricify Lines Creator/StringHelper.cpp
@@ -1,51 +1,101 @@
 #include "StringHelper.h"
 
-namespace StringHelper
+std::wstring StringHelper::StringToWstring(const std::string& str)
 {
-	/// <summary>
-	/// È¥³ý×Ö·û´®Ê×Î²µÄ¿Õ°××Ö·û
-	/// </summary>
-	std::string Trim(const std::string& str)
-	{
-		size_t first = str.find_first_not_of(" \t\n\r");
-		if (first == std::string::npos)
-			return "";
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	return converter.from_bytes(str);
+}
 
-		size_t last = str.find_last_not_of(" \t\n\r");
-		return str.substr(first, last - first + 1);
-	}
+/// <summary>
+/// È¥³ý×Ö·û´®Ê×Î²µÄ¿Õ°××Ö·û
+/// </summary>
+std::string StringHelper::Trim(const std::string& str)
+{
+	size_t first = str.find_first_not_of(" \t\n\r");
+	if (first == std::string::npos)
+		return "";
 
-	/// <summary>
-	/// È¥³ý×Ö·û´®¿ªÍ·µÄ¿Õ°××Ö·û
-	/// </summary>
-	std::string TrimStart(const std::string& str)
-	{
-		size_t first = str.find_first_not_of(" \t\n\r");
-		if (first == std::string::npos) {
-			return "";
-		}
-		return str.substr(first);
-	}
+	size_t last = str.find_last_not_of(" \t\n\r");
+	return str.substr(first, last - first + 1);
+}
 
-	/// <summary>
-	/// È¥³ý×Ö·û´®½áÎ²µÄ¿Õ°××Ö·û
-	/// </summary>
-	std::string TrimEnd(const std::string& str)
-	{
-		size_t last = str.find_last_not_of(" \t\n\r");
-		if (last == std::string::npos) {
-			return "";
-		}
-		return str.substr(0, last + 1);
-	}
+/// <summary>
+/// È¥³ý×Ö·û´®Ê×Î²µÄ¿Õ°××Ö·û
+/// </summary>
+std::wstring StringHelper::Trim(const std::wstring& str)
+{
+	size_t first = str.find_first_not_of(L" \t\n\r");
+	if (first == std::string::npos)
+		return L"";
 
-	/// <summary>
-	/// ÊÇ·ñÎª¿Õ°××Ö·û´®»ò½ö°üº¬¿Õ¸ñµÄ×Ö·û´®
-	/// </summary>
-	bool IsEmptyOrWhiteSpace(const std::string& str)
-	{
-		if (str.empty()) return true;
-		if (Trim(str).empty()) return true;
-		return false;
+	size_t last = str.find_last_not_of(L" \t\n\r");
+	return str.substr(first, last - first + 1);
+}
+
+/// <summary>
+/// È¥³ý×Ö·û´®¿ªÍ·µÄ¿Õ°××Ö·û
+/// </summary>
+std::string StringHelper::TrimStart(const std::string& str)
+{
+	size_t first = str.find_first_not_of(" \t\n\r");
+	if (first == std::string::npos) {
+		return "";
 	}
+	return str.substr(first);
+}
+
+/// <summary>
+/// È¥³ý×Ö·û´®¿ªÍ·µÄ¿Õ°××Ö·û
+/// </summary>
+std::wstring StringHelper::TrimStart(const std::wstring& str)
+{
+	size_t first = str.find_first_not_of(L" \t\n\r");
+	if (first == std::string::npos) {
+		return L"";
+	}
+	return str.substr(first);
+}
+
+/// <summary>
+/// È¥³ý×Ö·û´®½áÎ²µÄ¿Õ°××Ö·û
+/// </summary>
+std::string StringHelper::TrimEnd(const std::string& str)
+{
+	size_t last = str.find_last_not_of(" \t\n\r");
+	if (last == std::string::npos) {
+		return "";
+	}
+	return str.substr(0, last + 1);
+}
+
+/// <summary>
+/// È¥³ý×Ö·û´®½áÎ²µÄ¿Õ°××Ö·û
+/// </summary>
+std::wstring StringHelper::TrimEnd(const std::wstring& str)
+{
+	size_t last = str.find_last_not_of(L" \t\n\r");
+	if (last == std::string::npos) {
+		return L"";
+	}
+	return str.substr(0, last + 1);
+}
+
+/// <summary>
+/// ÊÇ·ñÎª¿Õ°××Ö·û´®»ò½ö°üº¬¿Õ¸ñµÄ×Ö·û´®
+/// </summary>
+bool StringHelper::IsEmptyOrWhiteSpace(const std::string& str)
+{
+	if (str.empty()) return true;
+	if (Trim(str).empty()) return true;
+	return false;
+}
+
+/// <summary>
+/// ÊÇ·ñÎª¿Õ°××Ö·û´®»ò½ö°üº¬¿Õ¸ñµÄ×Ö·û´®
+/// </summary>
+bool StringHelper::IsEmptyOrWhiteSpace(const std::wstring& str)
+{
+	if (str.empty()) return true;
+	if (Trim(str).empty()) return true;
+	return false;
 }
--- Lyricify Lines Creator/StringHelper.h
+++ Lyricify Lines Creator/StringHelper.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <string>
+#include <locale>
+#include <codecvt>
 
-namespace StringHelper
+class StringHelper
 {
-	std::string Trim(const std::string&);
-	std::string TrimStart(const std::string&);
-	std::string TrimEnd(const std::string&);
-	bool IsEmptyOrWhiteSpace(const std::string&);
-}
+public:
+	static std::wstring StringToWstring(const std::string& str);
+	static std::string Trim(const std::string&);
+	static std::wstring Trim(const std::wstring&);
+	static std::string TrimStart(const std::string&);
+	static std::wstring TrimStart(const std::wstring&);
+	static std::string TrimEnd(const std::string&);
+	static std::wstring TrimEnd(const std::wstring&);
+	static bool IsEmptyOrWhiteSpace(const std::string&);
+	static bool IsEmptyOrWhiteSpace(const std::wstring&);
+};
